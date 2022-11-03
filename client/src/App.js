723caf180546f3c0873cd3d2329b614a98a56620
Meka James <mekajames@Mekas-MBP.attlocal.net>
2022-11-03 10:35:04 -0500

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Unauthorized from './Components/Unauthorized'
 import Authorized from './Components/Authorized'
 
 function App() {
 
   const [auth, setAuth] = useState(false)
+// add use Effect hook
 
   const getAuth = async () => {
     try {
-      const data = await fetch('http://localhost:3543/api/')
+      const data = await fetch('http://localhost:3543/api/auth') // control url to serve auth
       const token = await data.json()
       localStorage.setItem('xAuthToken', token);
       if(localStorage.getItem('xAuthToken')) {
@@ -20,6 +21,7 @@ function App() {
       console.error(error.message)
     }
   }
+
 
   const logOut = () => {
     localStorage.removeItem('xAuthToken')
--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const {generate} = require('../utility/tokenGeneration')
+const generate = require('../utility/tokenGeneration') // destoryed the {}
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
         if(!req.headers.xToken) {
-            const number = generate()
+            const number = generate() // or add {} to generate
             return res.json({token: number}).status(200)
         }
 
